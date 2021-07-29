8dd0b11ff69dca5a2fb71c4182660a097702b288
Luanzeixon <luananjos765@gmail.com>
2021-07-29 16:52:34 -0300

    04_ImplementaçãoSecretario
    Implementação de secretario e correção de erros no professor.

--- 01_Projeto_Tcc/src/main/java/br/edu/ifrn/projetotcc/controller/cadastroSecretarioController.java
+++ 01_Projeto_Tcc/src/main/java/br/edu/ifrn/projetotcc/controller/cadastroSecretarioController.java
@@ -20,26 +20,26 @@ public class cadastroSecretarioController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping("/cadastroProfessor")
+	@GetMapping("/cadastroSecretario")
 	public String entrarCadastroProfessor(ModelMap model) {
 		model.addAttribute("usuario", new Usuario());
-		return "usuario/cadastroProfessor";
+		return "usuario/cadastroSecretario";
 	}
 	
-	@PostMapping("/salvarProfessor")
+	@PostMapping("/salvarSecretario")
 	@Transactional(readOnly = false)
-	public String salvarCadastroProfessor(Usuario usuario, RedirectAttributes attr) {
+	public String salvarCadastroSecretario(Usuario usuario, RedirectAttributes attr) {
 		
 		String senhaCriptografada = 
 				new BCryptPasswordEncoder().encode(usuario.getSenha());
 		usuario.setSenha(senhaCriptografada);
 		
-		usuario.setTipo("PROFESSOR");
+		usuario.setTipo("SECRETARIO");
 		
 		usuarioRepository.save(usuario);
 		
-		attr.addFlashAttribute("msgSucesso", "Estudante inserido com sucesso");
+		attr.addFlashAttribute("msgSucesso", "Secretario inserido com sucesso");
 
-		return "redirect:/usuarios/cadastroProfessor";
+		return "redirect:/usuarios/cadastroSecretario";
 	}
 }
