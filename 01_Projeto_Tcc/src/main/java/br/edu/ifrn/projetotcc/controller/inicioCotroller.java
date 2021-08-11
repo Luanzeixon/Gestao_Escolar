7c11c73a9429c5c41723c0ef978b98630272d307
Luanzeixon <luananjos765@gmail.com>
2021-08-11 11:40:24 -0300

    08_LoginRedirecionado
    Ao fazer o login(autenticacao) cada tipo de usuario é levado pra uma pagina de acordo.

--- 01_Projeto_Tcc/src/main/java/br/edu/ifrn/projetotcc/controller/inicioCotroller.java
+++ 01_Projeto_Tcc/src/main/java/br/edu/ifrn/projetotcc/controller/inicioCotroller.java
@@ -1,16 +1,42 @@
 package br.edu.ifrn.projetotcc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.edu.ifrn.projetotcc.dominio.Usuario;
+import br.edu.ifrn.projetotcc.repository.UsuarioRepository;
+
 @Controller
 public class inicioCotroller {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@GetMapping("/")
 	public String inicio() {
-
-		return "inicio";
+		
+		//BUSCANDO DADOS DO USUARIO LOGADO
+		Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+       
+        String email = authentication.getName();
+        Usuario usuario = usuarioRepository.findByEmail(email).get();
+        
+        if(usuario.getTipo().equals("ESTUDANTE")) {
+        	return "usuario/paginaEstudante";
+        }if(usuario.getTipo().equals("PROFESSOR")) {
+        	return "usuario/paginaProfessor";
+        }if(usuario.getTipo().equals("PARENTE")) {
+        	return "usuario/paginaParente";
+        }if(usuario.getTipo().equals("SECRETARIO") || usuario.getTipo().equals("ADM") ) {
+        	return "inicio";
+        }else{
+        	return "PaginaErro";
+        }
 	}
 	
 	@GetMapping("/login")
